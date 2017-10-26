f273654119dccb47a9aef14100514066a4ff8f0a
DakotaDecker <33061529+DakotaDecker@users.noreply.github.com>
2017-10-26 11:47:38 -0500

    Clarity

--- Additional Exercise One.py
+++ Additional Exercise One.py
@@ -1,13 +1,18 @@
 def initial(name):
+    # Grabs the first letter of a name and returns it appended with a period
     initial = list(name)
     initial = initial[0]
     initial = initial + '.'
     return initial
+
 fullName = input('Enter your first, middle, and last names, separated by a space:\n')
-full = fullName.split()
+
+full = fullName.split() # Splits user input into a list with three strings
+
 first = full[0]
 middle = full[1]
 last = full[2]
+
 print('First name:', first)
 print('Middle name:', middle)
 print('Last name:', last)
