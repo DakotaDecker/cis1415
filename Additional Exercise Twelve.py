d9a35c994d4c35b7b9317ce3368ee415cac3a016
DakotaDecker <33061529+DakotaDecker@users.noreply.github.com>
2017-10-26 11:53:36 -0500

    Added explanatory comments

--- Additional Exercise Twelve.py
+++ Additional Exercise Twelve.py
@@ -1,13 +1,13 @@
 def pig(english):
-    pigLatin = ''
-    phrase = english.split()
+    pigLatin = '' # Empty string to later contain full pig latin translation
+    phrase = english.split() # Splits english phrase into list of words
     for x in phrase:
-        word = list(x)
-        end = word[0] + 'ay'
-        del(word[0])
-        word.append(end)
-        piggy = ''.join(word)
-        pigLatin += piggy + ' '
+        word = list(x) # Splits word into list of letters
+        end = word[0] + 'ay' # Assigns the first letter of the word and the string 'ay' to 'end'
+        del(word[0]) # Removes the first letter from the word
+        word.append(end) # Appends the removed first letter and 'ay' to word
+        piggy = ''.join(word) # Joins the list of letters into one word
+        pigLatin += piggy + ' ' # Appends word to the end of the final pig latin translation along with a space
     return pigLatin
 
 user = input('Enter a phrase in English using just letters and spaces:\n')
