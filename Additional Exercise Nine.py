d0ac421d948d1db44a7acd3156fba3c46bfceaef
MindiCobb <33061611+MindiCobb@users.noreply.github.com>
2017-10-31 09:08:36 -0500

    Add files via upload

--- Additional Exercise Nine.py
+++ Additional Exercise Nine.py
@@ -1,28 +1,39 @@
-def vowel(string):
-    count = 0
-    string = string.lower()
-    for x in string:
-        if x == 'a':
-            count += 1
-        if x == 'e':
-            count += 1
-        if x == 'i':
-            count += 1
-        if x == 'o':
-            count += 1
-        if x == 'u':
-            count += 1
-    return count
+def num_vowels(UserInput):
+    num = UserInput.count('a')
+    num2 = UserInput.count('e')
+    num3 = UserInput.count('i')
+    num4 = UserInput.count('o')
+    num5 = UserInput.count('u')
+    return num + num2 + num3 + num4 + num5
 
-def con(string):
-    alpha = 0
-    for x in string:
-        if x.isalpha():
-            alpha += 1
-    con = alpha - vowel(user)
-    return con
+def num_consonants(userinput):
+    n1 = userinput.count('b')
+    n2 = userinput.count('c')
+    n3 = userinput.count('d')
+    n4 = userinput.count('f')
+    n5 = userinput.count('g')
+    n6 = userinput.count('h')
+    n7 = userinput.count('j')
+    n8 = userinput.count('k')
+    n9 = userinput.count('l')
+    n10 = userinput.count('m')
+    n11 = userinput.count('n')
+    n12 = userinput.count('p')
+    n13 = userinput.count('q')
+    n14 = userinput.count('r')
+    n15 = userinput.count('s')
+    n16 = userinput.count('t')
+    n17 = userinput.count('v')
+    n18 = userinput.count('w')
+    n19 = userinput.count('x')
+    n20 = userinput.count('y')
+    n21 = userinput.count('z')
+    return n1 + n2 +n3 + n4 + n5 + n6 +n7 + n8 + n9 + n10 + n11 + n12 + n13 + n14 + n15 + n16 + n17 + n18 + n19 + n20 + n21 
+    
 
-user = input('Enter a string:\n')
-
-print('\nString contains %d vowels.' % vowel(user))
-print('String contains %d consonants.' % con(user))
+if __name__ == '__main__':
+    Input = input('Enter a String:\n')
+    vowels = num_vowels(Input)
+    consonants = num_consonants(Input)
+    print('Number of vowels:', vowels)
+    print('Number of consonants:', consonants)
