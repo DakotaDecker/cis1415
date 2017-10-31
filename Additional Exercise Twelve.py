bf4205ca9a1554dda5fd2aeed5361ffefb0f22b7
DakotaDecker <33061529+DakotaDecker@users.noreply.github.com>
2017-10-31 09:35:12 -0500

    Update Additional Exercise Twelve.py

--- Additional Exercise Twelve.py
+++ Additional Exercise Twelve.py
@@ -1,1 +1,16 @@
+def pig(english):
+    pigLatin = ''
+    phrase = english.split()
+    for x in phrase:
+        word = list(x)
+        end = word[0] + 'ay'
+        del(word[0])
+        word.append(end)
+        piggy = ''.join(word)
+        pigLatin += piggy + ' '
+    return pigLatin
 
+user = input('Enter a phrase in English using just letters and spaces:\n')
+
+print('\nEnglish:', user)
+print('Pig Latin:', pig(user))
