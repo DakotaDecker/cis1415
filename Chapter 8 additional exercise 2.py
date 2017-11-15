9e5181a29fc8e91cd48d3c826e4014d161564395
MindiCobb <33061611+MindiCobb@users.noreply.github.com>
2017-11-14 21:29:07 -0600

    Add files via upload

---
+++ Chapter 8 additional exercise 2.py
@@ -0,0 +1,19 @@
+Num_list = [5, 10, 15, 20, 25]
+
+print(Num_list, '\n')
+print('Get an element from the list:', Num_list[2], '\n')
+Num_list [4] = 26
+print('Change a value of a certain element in the list:', Num_list, '\n')
+del Num_list[1]
+print(Num_list, '\n')
+
+Food_list = ['cake', 'cookies', 'cupcake', 'ice cream']
+
+print(Food_list, '\n')
+Food_list.append('pie')
+print('Add an item to the end of the list: \n', Food_list, '\n')
+Food_list.pop()
+print('Remove and return last item in list: \n', Food_list, '\n')
+Food_list.reverse()
+print('Reverse the elements of list in-place: \n', Food_list, '\n')
+
